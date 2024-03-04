4dcd23f424d6018b82335490165641d308ba2677
moonlapsed <MOONLAPSED@gmail.com>
2024-03-04 02:54:50 -0600

    init/setup.py + workflow + ran app (leaving-in redacted /logs/..logs

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import sys
+import subprocess
+
 from pydantic import BaseModel, Field, validator
 from dataclasses import dataclass, field
 import os
-import subprocess
 from src.lager import Lager  # capital "L"
-import sys
 import uuid
+import subprocess
 
 app = None  # Use the pydantic app from main
 lager = None  # Use the lager from main
+ml = Lager()  # Initialize ml as an instance of Lager
+
+try:
+    subprocess.run('git config --global --add safe.directory C:/Users/WDAGUtilityAccount/Desktop/')
+except Exception as e:
+    print("Failed to add exception for Git directory:", e)
+
 
 
 def hash():
@@ -24,20 +33,18 @@ def hash():
         else:
             raise  # Re-raise other Git errors for debugging
 
-
 def runtime():
     """runtime sets the environment variables and runs the application."""
     global app, lager
     lager = Lager()
-    lager.Lager.validate()  # Update and validate configuration
-
+    lager.validate()  # Update and validate configuration
     try:
         # Validate and retrieve a branch logger for main.py
-        ml = lager.Lager.branch_logger('main') # Retrieve a branch logger for main.py
+        ml = lager.branch_logger('main') # Retrieve a branch logger for main.py
         globals()['ml'] = ml  # Store in globals()
         return ml
-    except:
-        lager.Lager.error("Failed to retrieve Lager() branch.")
+    except Exception as e:
+        print(f"Failed to retrieve Lager() branch. Error: {e}")
         pass
     finally:
         ml.info("passing lager object to main()")
@@ -48,14 +55,14 @@ def main():
     """main is the entry point for the application."""
     global app, ml
     try:
-        ml = runtime()
+        ml = Lager.get_logger()  # Change this line
+        runtime()
     except:
         print("Failed to retrieve Lager() branch.")
         sys.exit(1)
     finally:
         ml.info("passing lager object to main()")
     return ml
-
 
 if __name__ == "__main__":
     try:
